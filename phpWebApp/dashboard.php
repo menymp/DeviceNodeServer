38c7b16158086ec1328cdf6d2ac7ccd78877e71d
menymp <lmeny04@hotmail.com>
2023-06-02 23:22:18 -0600

    improved logic for control creation

--- phpWebApp/dashboard.php
+++ phpWebApp/dashboard.php
@@ -57,6 +57,7 @@ function FetchControlsResponse()
 				$('#outputMessage').text(decodedData['Message']);
 			}
 			BuildControlApperance(decodedData);
+			initCommandsUpdate(); 
 		}
 	});
 }
@@ -78,43 +79,67 @@ function BuildControlApperance(data)
 	
 	for (var i = 0, len = data.length; i < len; i++) 
 	{
+		/*this is the proof of concept where each control has an object that defines apperance and behavior*/
+		/*ToDo: better to refine this design before more complex controls arrive*/
+		var controlParameters = JSON.parse(data[i]["parameters"]);
+		var ctrlName = data[i]["name"]
 		if(data[i]["typename"] === "DIGITALOUTPUT")
 		{
-			var controlParameters = JSON.parse(data[i]["parameters"]);
-			//alert(controlParameters["idDevice"]);
-			//alert("button");
-			//alert(data[i]["parameters"]);
-			var ControlElementContainer = document.createElement('form');
-			var ControlElementContainerText = document.createElement('label');
-			ControlElementContainer.innerHTML = data[i]["name"];
-			var toggleSw = document.createElement('label');
-				toggleSw.setAttribute('class','switch');
-			var tmpCheckBox = document.createElement('input');
-				tmpCheckBox.setAttribute('type','checkbox');
-				//alert(controlParameters["onCmdStr"]);
-				let tmpid = controlParameters["idDevice"];
-				let cmdOn = controlParameters["onCmdStr"];
-				let cmdOff = controlParameters["offCmdStr"];
-				
-				tmpCheckBox.onclick = function() { if(this.checked)
-														commandHandler(tmpid,cmdOn);
-												   else
-														commandHandler(tmpid,cmdOff);
-												};
-				tmpCheckBox.setAttribute('deviceId','1');
-			var tmpSpan = document.createElement('span');
-				tmpSpan.setAttribute('class','slider round');
-			
-			toggleSw.appendChild(tmpCheckBox);
-			toggleSw.appendChild(tmpSpan);
-			ControlElementContainer.appendChild(toggleSw);
+			var controlClass = ctrlDigitalOutput(ctrlName, controlParameters);
+			var ControlElementContainer = controlClass.constructUiApperance(commandHandler)
 			$("#controlsContainer").append(ControlElementContainer);
-				//toggleSw.id = 
-				//toggleSw.setAttribute('name', 'college[]');
-				//toggleSw.setAttribute('class', 'form-control input-sm');
-				//toggleSw.setAttribute('placeholder','Name of College/University');
-				//toggleSw.setAttribute('required','required');
+			controls.append(controlClass);
+			// var controlParameters = JSON.parse(data[i]["parameters"]);
+			// //alert(controlParameters["idDevice"]);
+			// //alert("button");
+			// //alert(data[i]["parameters"]);
+			// var ControlElementContainer = document.createElement('form');
+			// var ControlElementContainerText = document.createElement('label');
+			// ControlElementContainer.innerHTML = data[i]["name"];
+			// var toggleSw = document.createElement('label');
+				// toggleSw.setAttribute('class','switch');
+			// var tmpCheckBox = document.createElement('input');
+				// tmpCheckBox.setAttribute('type','checkbox');
+				// //alert(controlParameters["onCmdStr"]);
+				// let tmpid = controlParameters["idDevice"];
+				// let cmdOn = controlParameters["onCmdStr"];
+				// let cmdOff = controlParameters["offCmdStr"];
+				// let cmdRead = controlParameters["updateCmdStr"];/*add in objects template*/
+				// let cmdArgs = controlParameters["updateArgsStr"];
+				
+				// tmpCheckBox.onclick = function() { if(this.checked)
+														// commandHandler(tmpid,cmdOn);
+												   // else
+														// commandHandler(tmpid,cmdOff);
+												// };
+				// tmpCheckBox.setAttribute('deviceId',tmpid);
+			// var tmpSpan = document.createElement('span');
+				// tmpSpan.setAttribute('class','slider round');
 			
+			// toggleSw.appendChild(tmpCheckBox);
+			// toggleSw.appendChild(tmpSpan);
+			// ControlElementContainer.appendChild(toggleSw);
+			// $("#controlsContainer").append(ControlElementContainer);
+				// //toggleSw.id = 
+				// //toggleSw.setAttribute('name', 'college[]');
+				// //toggleSw.setAttribute('class', 'form-control input-sm');
+				// //toggleSw.setAttribute('placeholder','Name of College/University');
+				// //toggleSw.setAttribute('required','required');
+			// /*ToDo: maybe the device id is not needed since the updateCallback should handle the response*/
+			// const update = (control, response) => {
+				// checkBox = control.control.querySelectorAll('checkbox[deviceId]='+control.idDevice); //selects the checkbox
+				// let check = false;
+				// if(response.result === control.controlParameters['onCmdStr'])
+				// {
+					// check = true;
+				// }
+				// if(response.state === 'SUCCESS')
+				// {
+					// checkBox.checked = check
+				// }
+				
+			// }
+			// addControlUpdateCmd(toggleSw, tmpid, controlParameters, cmdRead, cmdArgs, updateCallback);
 		}
 		if(data[i]["typename"] === "PLAINTEXT")
 		{
@@ -153,7 +178,97 @@ function BuildControlApperance(data)
 }
 
 
-function commandHandler(deviceId,cmd)
+class ctrlDigitalOutput
+{
+	constructor(name, controlParameters)
+	{
+		this.name = name;
+		this.idDevice = controlParameters["idDevice"];
+		this.cmdOn = controlParameters["onCmdStr"];
+		this.cmdOff = controlParameters["offCmdStr"];
+		this.cmdUpdate = controlParameters["updateCmdStr"];/*add in objects template*/
+		this.cmdUpdateArgs = controlParameters["updateArgsStr"];
+	}
+	
+	constructUiApperance(usrCommandHandler)
+	{
+		var ControlElementContainer = document.createElement('form');
+		var ControlElementContainerText = document.createElement('label');
+		ControlElementContainer.innerHTML = this.name;
+		var toggleSw = document.createElement('label');
+		toggleSw.setAttribute('class','switch');
+		var tmpCheckBox = document.createElement('input');
+		tmpCheckBox.setAttribute('type','checkbox');
+		
+		tmpCheckBox.onclick = this.userClick(usrCommandHandler);
+		
+		tmpCheckBox.setAttribute('deviceId',this.idDevice);
+		var tmpSpan = document.createElement('span');
+		tmpSpan.setAttribute('class','slider round');
+			
+		toggleSw.appendChild(tmpCheckBox);
+		toggleSw.appendChild(tmpSpan);
+		ControlElementContainer.appendChild(toggleSw);
+		
+		//$("#controlsContainer").append(ControlElementContainer);
+		this.uiRefControl = tmpCheckBox;
+		
+		return ControlElementContainer;
+	}
+	
+	userClick(commandHandler)
+	{
+		if(this.uiRefControl.checked)
+			commandHandler(this.idDevice,this.cmdOn, "");/*ToDo: create cmd object*/
+		else
+			commandHandler(this.idDevice,this.cmdOff, "");/*ToDo: create cmd object*/
+	}
+	
+	update(response)
+	{
+		checkBox = this.control.querySelectorAll('checkbox[deviceId]='+this.idDevice); //selects the checkbox
+		let check = false;
+		if(response.result === this.control.controlParameters['onCmdStr'])
+		{
+			check = true;
+		}
+		if(response.state === 'SUCCESS')
+		{
+			checkBox.checked = check
+		}
+	}
+	/*ToDo: review if a best approach is to move this function to a super class*/
+	getUpdateCommand()
+	{
+		var cmdObj = new Object();
+		cmdObj.idDevice = this.idDevice;
+		cmdObj.command = this.cmdUpdate;
+		cmdObj.args = this.cmdUpdateArgs;
+		return cmdObj;
+	}
+}
+
+/*
+control reference to the control
+idDevice 		id of the hardware device identifier
+controlParameters complete parameters
+updateCommand 	a valid command to get an state update
+updateArgs 		add args if expected from device
+updateCallback	a function to perform when a response is received
+*/
+/*function addControlUpdateCmd(control, idDevice, updateCommand, controlParameters,  updateArgs, updateCallback)
+{
+	var control = new Object();
+	control.control = control; 
+	control.idDevice = idDevice;
+	control.controlParameters = controlParameters;
+	control.command = updateCommand;
+	control.args = updateArgs;
+	control.updateCallback = updateCallback;
+	controls.append(control);
+}*/
+
+function commandHandler(deviceId, cmd, args)
 {
 	// var deviceId = caller.event.target.getAttribute('deviceId');
 	//alert("command executed, device: "+deviceId + " cmd: "+ cmd);
@@ -161,7 +276,7 @@ function commandHandler(deviceId,cmd)
 	var cmdObj = new Object();
 	cmdObj.idDevice = deviceId;
 	cmdObj.command = cmd;
-	cmdObj.args = ""; //ToDo: remember what the f*** is this field for
+	cmdObj.args = args; //ToDo: remember what the f*** is this field for
 	// cmdObj.set('idDevice',deviceId);
 	// cmdObj.set('command',cmd);
 	// cmdObj.set('args','');
@@ -174,15 +289,11 @@ function commandHandler(deviceId,cmd)
 	//ws_send(cmdObj);
 }
 
-//ToDo: call it!!!
 function initCommandsUpdate()
 {
 	cmds = []
-	controls.forEach((command)=>{
-		var cmdObj = new Object();
-		cmdObj.idDevice = command.deviceId;
-		cmdObj.command = command.cmd;
-		cmdObj.args = command.args;
+	controls.forEach((controlClass)=>{
+		var cmdObj = controlClass.getUpdateCommand();
 		cmds.append(cmdObj);
 	});
 	controlsCommands = JSON.stringify(cmds);
@@ -200,6 +311,7 @@ function commandScheduler()
 	if(len(userCommands))
 	{
 		jsonStr = JSON.stringify(userCommands);
+		userCommands.empty();
 	}
 	else
 	{
@@ -217,7 +329,11 @@ function responseHandler(evt)
 	//process the response
 	var responses = JSON.parse(evt.data);
 	
-	//ToDo: process the response logic
+	responses.foreach((response) => {
+		/*get the corresponding class of the response and updates it*/
+		control = controls.filter((control) => control.idDevice == response.idDevice);
+		control.update(response);
+	});
 	
 	setTimeout(() => {
 		if(flagStop == 1)//ToDo: define flag stop
