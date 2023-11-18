e9a94593e7e5d86516070182fc1eb84e523757a1
menymp <lmeny04@hotmail.com>
2023-11-17 22:57:18 -0600

    update arduino example to new procedure

--- deviceNodeServer/xbeeNetwork/testScripts/arduinoDeviceTest/arduinoDeviceTest.ino
+++ deviceNodeServer/xbeeNetwork/testScripts/arduinoDeviceTest/arduinoDeviceTest.ino
@@ -12,12 +12,13 @@ XBeeResponse response = XBeeResponse();
 Rx16Response rx16 = Rx16Response();
 Rx64Response rx64 = Rx64Response();
 
-XBeeAddress64 addr64 = XBeeAddress64(0x00000000, 0x00000000);
+//XBeeAddress64 addr64 = XBeeAddress64(0x00000000, 0x00000000);
 const int sensorAnalogPin = 0;
 const int outputPin = 13;
 int cnt = 0;
 uint8_t data = 0;
 uint8_t option = 0;
+int request_len = 0;
 
 void setup() {
   pinMode(outputPin,OUTPUT);
@@ -38,26 +39,34 @@ void loop() {
         xbee.getResponse().getRx16Response(rx16);
         option = rx16.getOption();
         data = rx16.getData(0);
+        request_len = 16;
       } 
       else 
       {
         xbee.getResponse().getRx64Response(rx64);
         option = rx64.getOption();
         data = rx64.getData(0);
+        request_len = 64;
       }
 
       if(data == 'a') digitalWrite(outputPin, HIGH);
       if(data == 'b') digitalWrite(outputPin, LOW);
+
+      //digitalWrite(outputPin,!digitalRead(outputPin));
+      if(request_len == 64)
+      {
+        String bufferOut = "sensorRead:" + String(analogRead(sensorAnalogPin)) + ",output:" + String(digitalRead(outputPin)) + "\n";
+        Tx64Request tx = Tx64Request(rx64.getRemoteAddress64(), bufferOut.c_str(), bufferOut.length());
+        xbee.send(tx);
+      }
+      if(request_len == 16)
+      {
+        String bufferOut = "sensorRead:" + String(analogRead(sensorAnalogPin)) + ",output:" + String(digitalRead(outputPin)) + "\n";
+        Tx16Request tx = Tx16Request(rx16.getRemoteAddress16(), bufferOut.c_str(), bufferOut.length()); 
+        xbee.send(tx);
+      }
+
     }
   }
-  if(cnt > 60)
-  {
-    digitalWrite(outputPin,!digitalRead(outputPin));
-    String bufferOut = "sensorRead:" + String(analogRead(sensorAnalogPin)) + ",output:" + String(digitalRead(outputPin)) + "\n";
-    Tx64Request tx = Tx64Request(addr64, bufferOut.c_str(), bufferOut.length()); //ToDo: set modem address as static
-    xbee.send(tx);
-    cnt = 0;
-  }
-  cnt ++;
   //delay(10);
 }
