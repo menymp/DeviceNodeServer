6708ada9213ed9ae515fb3a80ce36bbba527f690
menymp <lmeny04@hotmail.com>
2023-11-30 22:40:12 -0600

    add server start commands

--- deviceNodeServer/DeviceMainNodeServer/serverStart.py
+++ deviceNodeServer/DeviceMainNodeServer/serverStart.py
@@ -7,13 +7,14 @@ menymp
 import subprocess
 import sys
 import json
+import signal
+from threading import Event
 
 sys.path.append('../ConfigsUtils')
 
 def loadCoreProcesses(filePath = './processList.json'):
     # Opening JSON file
     f = open(filePath)
- 
     # returns JSON object as 
     # a dictionary
     return json.load(f)["processes"]
@@ -35,18 +36,19 @@ def tryRunnProcesses(plist):
     pRunning = 0
     for process in plist:
         if process["state"] == "stop" and checkDependences(process, plist):
-            '''
-            subprocess.Popen(process["path"], shell=True)
-            process.terminate()   #Send SIGTERM
-            '''
-            #subprocess.run(process["path"], shell=False)
-            print("subprocess '" + process["path"] + "' started")
+            process["pipe"] = subprocess.Popen(process["path"], shell=True)
+            print("subprocess '" + str(process["path"]) + "' started")
             #ToDo: set an ack flag from each process to the output to determie if
             #      the process started correctly
             process["state"] = "running"
         else:
             pRunning = pRunning + 1
     return pRunning
+
+def stopProcesses(plist):
+    for process in plist:
+        process["pipe"].send_signal(signal.CTRL_BREAK_EVENT)
+    pass
 
 #The Idea behind this simple algoritm is to start the less dependent process first
 #   for each iteration, new processes can be started once its dependences are meet
@@ -57,4 +59,15 @@ if __name__ == "__main__":
     while pCount != pRunning:
         pRunning = tryRunnProcesses(plist)
     print("started '" + str(pRunning) + "' processes")
+
+    eventStop = Event()
+    def sigterm_handler(signum, frame):
+        print("stop process")
+        eventStop.set()
+        stopProcesses(plist)
+    signal.signal(signal.SIGINT, sigterm_handler)
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
+    while not eventStop.is_set():
+        pass
     pass
