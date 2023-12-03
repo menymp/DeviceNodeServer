9bfd6947e33743d0c4daf7695f8fa13c7b597d86
menymp <lmeny04@hotmail.com>
2023-12-02 22:45:09 -0600

    add rtk query and user services

---
+++ nodePackages/device-mgr-ui/src/services/userService.ts
@@ -0,0 +1,55 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export type loginUserInfo = {
+    mailuid: string;
+    pwd: string;
+}
+
+export type userInfo = {
+    username:string;
+    email:string;
+    registerdate:string;
+    telegrambotToken:string;
+}
+
+// Define a service using a base URL and expected endpoints
+export const userService = createApi({
+  reducerPath: 'userApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'localhost:8080/DeviceNodeServer/phpWebApp/' }),
+  endpoints: (builder) => ({
+    loginUser: builder.mutation<void , loginUserInfo>({
+      query: (usrInfo) => ({
+        url: 'userService.php',
+        method: 'POST',
+        body: {...usrInfo, type:"login"}
+      }),
+    }),
+    logoutUser: builder.mutation<void , void>({
+        query: () => ({
+          url: 'userService.php',
+          method: 'POST',
+          body: { type:"logout"}
+        }),
+    }),
+    getUserInfo: builder.mutation<userInfo , void>({
+        query: () => ({
+          url: 'userInfoService.php',
+          method: 'POST',
+          body: { type:"fetchUserInfo"},
+          transformResponse: (response: Array<any>) => response[0]
+        }),
+    }),
+    updateUserInfo: builder.mutation<void , userInfo>({
+        query: () => ({
+          url: 'userInfoService.php',
+          method: 'POST',
+          body: { type:"setUserInfo"}
+        }),
+    }),
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useLoginUserMutation, useLogoutUserMutation, useGetUserInfoMutation, useUpdateUserInfoMutation } = userService
