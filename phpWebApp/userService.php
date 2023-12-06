99f2d57d348c8e6d5ae5b5f70045af892aa88236
menymp <lmeny04@hotmail.com>
2023-12-05 20:33:46 -0600

    add basic cors routine for dev

--- phpWebApp/userService.php
+++ phpWebApp/userService.php
@@ -11,6 +11,25 @@ this is a service developed to compatibility for the new react UI
 
 a lot of work needs to be done for this to be minimaly operable, under construction
  */
+/* Handle CORS */
+
+// Specify domains from which requests are allowed
+header('Access-Control-Allow-Origin: *');
+
+// Specify which request methods are allowed
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+
+// Additional headers which may be sent along with the CORS request
+header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Content-Type');
+
+// Set the age to 1 day to improve speed/caching.
+header('Access-Control-Max-Age: 86400');
+
+// Exit early so the page isn't fully loaded for options requests
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'options') {
+    exit();
+}
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
