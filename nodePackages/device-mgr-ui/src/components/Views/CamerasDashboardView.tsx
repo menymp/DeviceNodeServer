78c3bdea09796a39711c352dff9a6741c28213b9
menymp <lmeny04@hotmail.com>
2024-02-07 23:22:46 -0600

    add initial structure for video dash

--- nodePackages/device-mgr-ui/src/components/Views/CamerasDashboardView.tsx
+++ nodePackages/device-mgr-ui/src/components/Views/CamerasDashboardView.tsx
@@ -1,21 +1,72 @@
 import React from "react";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Container, Row, Col, Button, Form, Modal } from 'react-bootstrap';
 import BaseTable from '../Table/Table'
 import { useNavigate } from "react-router-dom";
 import WebSocket from 'ws';
+import { dashboardCameraConfigs, useFetchConfigsMutation } from "../../services/camerasDashboardService";
+import { ITEM_LIST_DISPLAY_CNT } from '../../constants';
 
+const initialTableState = {
+    headers: ['Node id', 'Name', 'Path', 'Protocol', 'Owner', 'Parameters'],
+    rows: [],
+    detailBtn: false,
+    deleteBtn: false,
+    editBtn: false,
+}
 
 const CamerasDashboardView: React.FC = () => {
     const [show, setShow] = useState(false);
     const navigate = useNavigate();
+    const [getConfigsFetch, {isSuccess: configsLoaded, data: dashConfigs}] = useFetchConfigsMutation();
+    const [page, setPage] = useState<number>(0);
+    const [startVideo, setStartVideo] = useState<boolean>(false);
+
+    const [selectedConfig, setSelectedConfig] = useState<dashboardCameraConfigs>();
+    const handleChangeConfig = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        if (!configsLoaded || !dashConfigs || !event.target.value) {
+            return;
+        }
+        const selectedConfigTmp = dashConfigs?.find((config) => config.idvideoDashboard === parseInt(event.target.value))
+        if (selectedConfigTmp) {
+            setSelectedConfig(selectedConfigTmp)
+        }
+    }
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    useEffect(() => {
+        getConfigsFetch({pageCount: page, pageSize: ITEM_LIST_DISPLAY_CNT});
+    },[page]);
+    
+    const handleStartVideo = () => {
+        setStartVideo(true);
+        videoLoopStart();
+    }
+
+    const handleStopVideo = () => {
+        setStartVideo(false);
+    }
+
+    const getFrame = () => {
+        var image = document.getElementById("videofield");
+        if (!image || !selectedConfig) {
+            return;
+        }
+        image.setAttribute('src', "http://localhost:9090/video_feed?vidArgs="+JSON.stringify(selectedConfig.configJsonFetch));
+    }
+
+    const videoLoopStart = () => {
+        getFrame();
+        setTimeout(() => {
+            startVideo && videoLoopStart();
+        }, 10);
+    }
     // a pagination item already exists
 /*
     let initObj;
-let flagStop = 0;
+
 
 $(document).ready(function () {
 	fetchVideoConfigResponse();
@@ -47,12 +98,6 @@ function showViewForm()
 {
 	$('#newConfigForm').show();
 	$('#videofield').hide();
-}
-
-function getFrame()
-{
-	var image = document.getElementById("videofield");
-	image.src = "http://localhost:9090/video_feed?vidArgs="+JSON.stringify(initObj[0]['configJsonFetch']);
 }
 	
 function videoLoopStart() {
@@ -139,18 +184,32 @@ function fetchVideoConfigResponse()
                     <Col xs={2}>
                         <Button onClick={handleShow}>Editor</Button>
                     </Col>
+                    <Col xs={2}>
+                        <Button onClick={handleStartVideo}>Start</Button>
+                    </Col>
+                    <Col xs={2}>
+                        <Button onClick={handleStopVideo}>Stop</Button>
+                    </Col>
+                    <Col xs={2}>
+                        <Form.Group className="mb-3" controlId="nodeDetails.protocol">
+                            <Form.Label>Node protocol</Form.Label>
+                            <Form.Select onChange={handleChangeConfig} aria-label="MQTT" id="selectedConfigItem">
+                                {dashConfigs?.map((value, index) => { return <option id={`${index}`} value={value.idvideoDashboard}>{value.idvideoDashboard}</option>})}
+                            </Form.Select>
+                        </Form.Group>
+                    </Col>
                     <Col>
                         <Form className="mr-left ">
                             <Form.Group className="mb-3 form-check-inline" controlId="searchFilterField">
                                 <Row xs={12}>
                                     <Col xs={5}>
-                                        <Button>Next page</Button>
+                                        <Button onClick={() => {(page > 0) && setPage(page - 1)}}>Previous page</Button>
                                     </Col>
                                     <Col xs={1}>
-                                        <Form.Label> 10 </Form.Label>
+                                        <Form.Label>{page}</Form.Label>
                                     </Col>
                                     <Col xs={6}>
-                                        <Button>Previous page</Button>
+                                        <Button onClick={() => {setPage(page + 1)}}>Next page</Button>
                                     </Col>
                                 </Row>
                             </Form.Group>
@@ -158,6 +217,7 @@ function fetchVideoConfigResponse()
                     </Col>
                 </Row>
                 <Row>
+                    <img src={require('./styles/images/deadline.png')} id="videofield" />
                 </Row>
             </Container>
         </>
