053f33dff86d48a7d66027941d1f87bba7dbc5da
menymp <lmeny04@hotmail.com>
2024-03-18 22:10:46 -0600

    add base sensor read, add gauge lib

---
+++ nodePackages/device-mgr-ui/src/components/Views/controls/SensorRead.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { useState, useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
+import { Container, Row, Col, Button, Form, Modal } from 'react-bootstrap';
+import { 
+    GenericUIControlParameters, 
+    updateResponse, 
+    uiControlResponseHandler, 
+    deviceCommand, 
+    generateUpdateCommand, 
+    ws_send 
+} from '../../../types/ControlTypes'
+import GaugeChart from 'react-gauge-chart'
+import { POLL_INTERVAL_MS } from '../../../constants'
+
+type PlainTextParameters = {
+    idDevice: string, 
+    updateCmdStr: string, 
+    apperance: string
+}
+
+
+
+const SensorRead = (props: GenericUIControlParameters) => {
+    const { control } = props;
+    const [userCommands, setUserCommands] = useState<Array<deviceCommand>>([]);
+    const [currentValue, setCurrentValue] = useState<string>('');
+    const currentTimeout = useRef<NodeJS.Timeout>();
+    const responseTimeout = useRef<NodeJS.Timeout>();
+
+    const getControlParameters = () => {
+        return JSON.parse(control.parameters) as PlainTextParameters;
+    }
+    const parameters = getControlParameters();
+    const updateCommand = JSON.stringify([generateUpdateCommand(parseInt(parameters.idDevice), parameters.updateCmdStr, "")]);
+
+    useEffect(() => {
+        // run command scheduler for the first time
+        props.ws.addEventListener('message' , (evt) => {
+            uiControlResponseHandler(evt,  parseInt(parameters.idDevice), update);
+        });
+    
+        commandScheduler();
+    }, []);
+
+    const commandScheduler = () => {
+        //if user data
+        let jsonStr = "";
+        if(userCommands.length > 0)
+        {
+            jsonStr = JSON.stringify({ cmds: userCommands });
+            setUserCommands([]);
+        }
+        else
+        {
+
+            jsonStr = updateCommand; //already stringified
+        }
+        ws_send(props.ws, jsonStr);
+        responseTimeout.current = setTimeout(() => {
+            commandScheduler();
+        }, 3*POLL_INTERVAL_MS);
+    }
+
+    const update = (response: updateResponse) => {
+        if (currentTimeout.current) {
+            clearTimeout(currentTimeout.current);
+        }
+        if (responseTimeout.current) {
+            clearTimeout(responseTimeout.current);
+        }
+
+        if(response.state === 'SUCCESS')
+        {
+            setCurrentValue(response.result);
+        }
+        currentTimeout.current = setTimeout(() => {
+            commandScheduler();
+        }, POLL_INTERVAL_MS);
+    }
+    
+    return (
+        <>
+            <Form>
+                <Form.Label>{control.name}</Form.Label>
+                <GaugeChart id="gauge-chart3" 
+                    nrOfLevels={30} 
+                    colors={["#FF5F6D", "#FFC371"]} 
+                    arcWidth={0.3} 
+                    percent={0.37} 
+                />
+                <Form.Control // prettier-ignore
+                    type="switch"
+                    id={`plain-text${control.idControl}`}
+                    value={currentValue}
+                />
+            </Form>
+        </>
+    )
+}
+
+export default SensorRead;
