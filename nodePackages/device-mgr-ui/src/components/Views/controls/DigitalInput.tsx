1e31a62651231d9c87d774a2bfb7a9cd57d344bf
menymp <lmeny04@hotmail.com>
2024-05-06 20:45:38 -0600

    refract some components to custom hook

--- nodePackages/device-mgr-ui/src/components/Views/controls/DigitalInput.tsx
+++ nodePackages/device-mgr-ui/src/components/Views/controls/DigitalInput.tsx
@@ -1,16 +1,11 @@
-import React from "react";
-import { useState, useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
-import { Container, Row, Col, Button, Form, Modal } from 'react-bootstrap';
+import { useState } from 'react';
+import { Form } from 'react-bootstrap';
 import ColoredLed from 'colored-led';
 import { 
     GenericUIControlParameters, 
-    updateResponse, 
-    uiControlResponseHandler, 
-    deviceCommand, 
-    generateUpdateCommand, 
-    ws_send 
+    updateResponse
 } from '../../../types/ControlTypes'
-import { POLL_INTERVAL_MS } from '../../../constants'
+import useControlUtils from "../../../hooks/useControlUtils";
 
 type DigitalInputParameters = {
     idDevice: string, 
@@ -23,62 +18,22 @@ type DigitalInputParameters = {
 
 const DigitalInput = (props: GenericUIControlParameters) => {
     const { control } = props;
-    const [userCommands, setUserCommands] = useState<Array<deviceCommand>>([]);
     const [currentValue, setCurrentValue] = useState<string>('');
-    const currentTimeout = useRef<NodeJS.Timeout>();
-    const responseTimeout = useRef<NodeJS.Timeout>();
 
     const getControlParameters = () => {
         return JSON.parse(control.parameters) as DigitalInputParameters;
     }
     const parameters = getControlParameters();
-    const updateCommand = JSON.stringify([generateUpdateCommand(parseInt(parameters.idDevice), parameters.updateCmdStr, "")]);
-    const ledState = currentValue === parameters.onValue
-
-    useEffect(() => {
-        // run command scheduler for the first time
-        props.ws.addEventListener('message' , (evt) => {
-            uiControlResponseHandler(evt,  parseInt(parameters.idDevice), update);
-        });
-    
-        commandScheduler();
-    }, []);
-
-    const commandScheduler = () => {
-        //if user data
-        let jsonStr = "";
-        if(userCommands.length > 0)
-        {
-            jsonStr = JSON.stringify({ cmds: userCommands });
-            setUserCommands([]);
-        }
-        else
-        {
-
-            jsonStr = updateCommand; //already stringified
-        }
-        ws_send(props.ws, jsonStr);
-        responseTimeout.current = setTimeout(() => {
-            commandScheduler();
-        }, 3*POLL_INTERVAL_MS);
-    }
+    const ledState = currentValue === parameters.onValue;
 
     const update = (response: updateResponse) => {
-        if (currentTimeout.current) {
-            clearTimeout(currentTimeout.current);
-        }
-        if (responseTimeout.current) {
-            clearTimeout(responseTimeout.current);
-        }
-
         if(response.state === 'SUCCESS')
         {
             setCurrentValue(response.result);
         }
-        currentTimeout.current = setTimeout(() => {
-            commandScheduler();
-        }, POLL_INTERVAL_MS);
     }
+
+    const commandHandler = useControlUtils({ getControlParameters, ws: props.ws, update});
     
     return (
         <>
--- nodePackages/device-mgr-ui/src/components/Views/controls/SensorRead.tsx
+++ nodePackages/device-mgr-ui/src/components/Views/controls/SensorRead.tsx
@@ -1,19 +1,12 @@
-import React from "react";
-import { useState, useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
-import { Container, Row, Col, Button, Form, Modal } from 'react-bootstrap';
+import { useState } from 'react';
+import { Form } from 'react-bootstrap';
 import { 
     GenericUIControlParameters, 
-    updateResponse, 
-    uiControlResponseHandler, 
-    deviceCommand, 
-    generateUpdateCommand, 
-    ws_send 
+    updateResponse
 } from '../../../types/ControlTypes'
-import GaugeChart from 'react-gauge-chart'
-import { POLL_INTERVAL_MS } from '../../../constants'
-import { param } from "jquery";
 import { Gauge } from 'react-circular-gauge';
 import chroma from 'chroma-js';
+import useControlUtils from "../../../hooks/useControlUtils";
 
 type SensorReadParameters = {
     idDevice: string, 
@@ -28,61 +21,20 @@ type SensorReadParameters = {
 
 const SensorRead = (props: GenericUIControlParameters) => {
     const { control } = props;
-    const [userCommands, setUserCommands] = useState<Array<deviceCommand>>([]);
     const [currentValue, setCurrentValue] = useState<string>('');
-    const currentTimeout = useRef<NodeJS.Timeout>();
-    const responseTimeout = useRef<NodeJS.Timeout>();
 
     const getControlParameters = () => {
         return JSON.parse(control.parameters) as SensorReadParameters;
     }
     const parameters = getControlParameters();
-    const updateCommand = JSON.stringify([generateUpdateCommand(parseInt(parameters.idDevice), parameters.updateCmdStr, "")]);
-
-    useEffect(() => {
-        // run command scheduler for the first time
-        props.ws.addEventListener('message' , (evt) => {
-            uiControlResponseHandler(evt,  parseInt(parameters.idDevice), update);
-        });
-    
-        commandScheduler();
-    }, []);
-
-    const commandScheduler = () => {
-        //if user data
-        let jsonStr = "";
-        if(userCommands.length > 0)
-        {
-            jsonStr = JSON.stringify({ cmds: userCommands });
-            setUserCommands([]);
-        }
-        else
-        {
-
-            jsonStr = updateCommand; //already stringified
-        }
-        ws_send(props.ws, jsonStr);
-        responseTimeout.current = setTimeout(() => {
-            commandScheduler();
-        }, 3*POLL_INTERVAL_MS);
-    }
 
     const update = (response: updateResponse) => {
-        if (currentTimeout.current) {
-            clearTimeout(currentTimeout.current);
-        }
-        if (responseTimeout.current) {
-            clearTimeout(responseTimeout.current);
-        }
-
         if(response.state === 'SUCCESS')
         {
             setCurrentValue(response.result);
         }
-        currentTimeout.current = setTimeout(() => {
-            commandScheduler();
-        }, POLL_INTERVAL_MS);
     }
+    const commandHandler = useControlUtils({ getControlParameters, ws: props.ws, update});
     /*
                     <GaugeChart id= 
                     nrOfLevels={30} 
